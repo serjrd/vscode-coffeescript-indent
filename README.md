b0725c6913b5b974f78e5b1eae68b073d501fc47
serjrd <serjrd@gmail.com>
2020-07-28 19:56:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# coffeescript-indent
+
+Updated version of corpsyal.coffee-auto-indent-0.1.0
+
+## Features
+
+Auto indent on press enter after keywords like if, else if, else, switch, when, default, ()-> ..
+
+![feature X](https://image.ibb.co/fDC6xx/feature_x.gif)
---
+++ extension.js
@@ -0,0 +1,24 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate() {
+  vscode.languages.setLanguageConfiguration('coffeescript', {
+    indentationRules: {
+      increaseIndentPattern:
+        /^\s*(if .*|else|else if .*|switch .*|when .*|default|try|catch\s?.*|.* \-\>|.* \=\>|.* \=|.* \[|.+:)\s*$/,
+      decreaseIndentPattern:
+        /(else|when|catch)/,
+      unIndentedLinePattern: /(else|when|catch)/
+    }
+  })
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
